9345d9c7f73713f82bada21454993ac40dcde581
Sproc01 <95143387+Sproc01@users.noreply.github.com>
2023-02-28 22:45:54 +0100

    Update Model.py

--- Model.py
+++ Model.py
@@ -14,7 +14,7 @@ def obj_rule(model):
     return sum(model.y[f] for f in model.Trips)-1
 
 def constr_rule1(model):
-    return sum(model.x[i,"s",0] for i in model.Nodes)==model.Nodes.size()
+    return sum(model.x[i,"s",0] for i in model.Nodes)==len(Nodes)
 
 def constr_rule2(model):
     return sum(model.x[i,"d",2*len(model.Nodes)+1] for i in model.Nodes)+sum(model.x[i,"b", 2*len(model.Nodes)+1] for i in model.Nodes)==model.Nodes.size()
