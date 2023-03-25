f06aa8cb4780452b9d8bdabf9fea936393e78cf1
Sproc01 <95143387+Sproc01@users.noreply.github.com>
2023-03-25 13:05:36 +0100

    Aggiornamento converter a .dat

--- Generatore grafi/Converter.py
+++ Generatore grafi/Converter.py
@@ -4,16 +4,29 @@ files=os.listdir("Grafi")
 files.remove(".DS_Store")
 for i in files:
     file=open("Grafi/"+i, "r")
-    w=open("Convertiti/vertex_"+i, "w")
-    w1=open("Convertiti/edge_"+i, "w")
+    w=open("Convertiti/data_"+i[:len(i)-4]+".dat", "w")
     con=file.read()
     file.close()
+    w.write("set Places := s b d ;\n")
+    w.write("set Nodes := ")
     l=con.split("\n")
+    cont=0
+    for i in l:
+        if len(i)>0:
+            w.write(i[0]+" ")
+            cont+=1
+    w.write(";\nset Edges:= ")
     for i in l:
         l1=i.split(" ")
-        w.write(l1[0]+" ")
         for i in range(1, len(l1)):
-            w1.write(str(l1[0])+"-"+str(l1[i])+" ")
+            w.write(str(l1[0])+"-"+str(l1[i])+" ")
+    w.write(";\n")
+    w.write("set Trips := ")
+    for i in range(0,2*cont+2):
+        w.write(str(i)+" ")
+    w.write(";\n\n")
+    w.write("param Capacity := 1;\n")
+    w.write("param len := "+str(cont)+";\n")
     w.close()
-    w1.close()
     
+        
