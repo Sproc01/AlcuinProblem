7371addde241bd83e87e2b056db1278652fe5c7b
Sproc01 <95143387+Sproc01@users.noreply.github.com>
2023-03-28 15:24:20 +0200

    Update GraphGenerator.py

--- Generatore grafi/GraphGenerator.py
+++ Generatore grafi/GraphGenerator.py
@@ -3,14 +3,14 @@ import networkx as nx
 #Il metodo di NetworkX utilizzato nella generazione di questa tipologia di grafi e' 
 #gnprandomgraph(n,p,seed), che restituisce un’istanza di grafo G(n,p) di dimensione n in cui gli archi 
 #hanno probabilita` di essere generati pari a p, mentre il parametro seed regola il comportamento 
-#pseudo-casuale, in modo tale da permettere la riproduci- bilita` degli esperimenti.
+#pseudo-casuale, in modo tale da permettere la riproducibilita` degli esperimenti.
 G1=nx.gnp_random_graph(100,0.1)
 nx.write_adjlist(G1, "Grafi/gnp_random.txt")
 
 #Il metodo di NetworkX utilizzato nella generazione di questa tipologia di grafi e' 
 #random regular graph(d, n, seed), che restituisce un’istanza di grafo regolare di dimensione n 
 #in cui tutti i nodi hanno grado d. Il parametro seed anche in questo caso regola il
-#comportamento pseudo-casuale della generazione, permettendo la replicabilit`a dell’esperimento.
+#comportamento pseudo-casuale della generazione, permettendo la replicabilita' dell’esperimento.
 G2=nx.random_regular_graph(10, 100)
 nx.write_adjlist(G2, "Grafi/random_regular.txt")
 
@@ -29,7 +29,7 @@ nx.write_adjlist(G3, "Grafi/watts_strogatz.txt")
 #di dimensione n in cui ogni nuovo nodo viene collegato nel procedimento iterativo a m nodi esistenti. 
 #Anche in questo caso il parametro seed regola il comportamento casuale del- la generazione, 
 #agendo in particolare sulla scelta dell’insieme di archi da creare ad ogni iterazione, 
-#permettendo cosi' la replicabilit`a dell’esperimento.
+#permettendo cosi' la replicabilita' dell’esperimento.
 G4=nx.barabasi_albert_graph(100, 10)
 nx.write_adjlist(G4, "Grafi/barbasi_albert.txt")
 
