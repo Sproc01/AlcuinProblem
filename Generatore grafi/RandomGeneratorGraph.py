9d1a7750730ce36184f7af2187a815160fafda84
Sproc01 <95143387+Sproc01@users.noreply.github.com>
2023-04-11 19:42:24 +0200

    prova regular

--- Generatore grafi/RandomGeneratorGraph.py
+++ Generatore grafi/RandomGeneratorGraph.py
@@ -4,6 +4,7 @@ import networkx as nx
 #gnprandomgraph(n,p,seed), che restituisce un’istanza di grafo G(n,p) di dimensione n in cui gli archi 
 #hanno probabilita` di essere generati pari a p, mentre il parametro seed regola il comportamento 
 #pseudo-casuale, in modo tale da permettere la riproducibilita` degli esperimenti.
+
 i=0.1
 while i<1:
     G1=nx.gnp_random_graph(150,i)
@@ -15,3 +16,20 @@ while i<1:
     file.write("\n!"+str(c))
     file.close()
     i+=0.1
+
+
+#Il metodo di NetworkX utilizzato nella generazione di questa tipologia di grafi e' 
+#random regular graph(d, n, seed), che restituisce un’istanza di grafo regolare di dimensione n 
+#in cui tutti i nodi hanno grado d. Il parametro seed anche in questo caso regola il
+#comportamento pseudo-casuale della generazione, permettendo la replicabilita' dell’esperimento.
+i=1
+while i<10:
+    G1=nx.random_regular_graph(i, 100)
+    nx.write_adjlist(G1, "GrafiCasuali/Graph"+str(i)+".txt")
+    #capacita' barca: nx.approximation.maximum_independent_set(G1) Returns an approximate maximum independent set.
+    c=len(G1)-len(nx.approximation.maximum_independent_set(G1))
+    #scrivo valore su file
+    file=open("GrafiCasuali/Graph"+str(i)+".txt","a")
+    file.write("\n!"+str(c))
+    file.close()
+    i+=1
