e6430a5aba296acdf323b8b6cf446fa8faea69f1
Sproc01 <95143387+Sproc01@users.noreply.github.com>
2023-07-06 23:47:14 +0200

    execution to find difficult instances

--- Model.py
+++ Model.py
@@ -109,11 +109,13 @@ if __name__ == '__main__':
         v=0
     #opt.write("Modelli_generati/AlcuinAbstract_"+str(s[len(s)-v:len(s)-4])+".lp")
     res = opt.solve(tee=True)
-    file=open("file.csv","a")
-    file.write(str(s[len(s)-v:len(s)-4])+","+get_info_from_results(res, 'Time: ')+","+get_info_from_results(res, "lower bound: ")+","+get_info_from_results(res,"upper bound: ")+","+get_info_from_results(res, "termination condition: ")+"\n")
+    file=open("file1.csv","a")
+    file.write(str(s[len(s)-v:len(s)-4])+","+get_info_from_results(res, 'Time: ')+","+get_info_from_results(res, "lower bound: ")+","+get_info_from_results(res,"upper bound: ")+","+get_info_from_results(res, "termination condition: ")+','+str(instance.Capacity.value)+','+str(instance.len.value)+"\n")
     file.close()
     #for p in instance.x:
 	    #print("x[{}] = {}".format(p, value(instance.x[p])))
     for p in instance.y:
+        if value(instance.y[p])==0:
+            break
         print("y[{}] = {}".format(p, value(instance.y[p])))
     
