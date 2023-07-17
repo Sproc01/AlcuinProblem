1f726bea45de12ad8e5febe486f85bfc361d4f68
Sproc01 <95143387+Sproc01@users.noreply.github.com>
2023-07-17 21:01:39 +0200

    more 0.9 random graph

--- Generatore grafi/RandomGeneratorDifficultGraph.py
+++ Generatore grafi/RandomGeneratorDifficultGraph.py
@@ -4,16 +4,17 @@ z=0
 k=100
 p=0.9
 seed=0
-while z<10:
-    #while seed<40:
-    G1=nx.gnp_random_graph(k,p,seed=seed)
-    filename='GrafiCasuali/Graph'+str(int(p*100))+'_'+str(z)+'_'+str(seed)+'.txt'
-    nx.write_adjlist(G1,filename)
-    #capacita' barca: nx.approximation.maximum_independent_set(G1) Returns an approximate maximum independent set.
-    c=len(G1)-len(nx.approximation.maximum_independent_set(G1))-z
-    #scrivo valore su file
-    file=open(filename,"a")
-    file.write("\n!"+str(c))
-    file.close()
-    #seed+=1
+while z<2:
+    while seed<20:
+        G1=nx.gnp_random_graph(k,p,seed=seed)
+        filename='GrafiCasuali/Graph'+str(int(p*100))+'_'+str(z)+'_'+str(seed)+'.txt'
+        nx.write_adjlist(G1,filename)
+        #capacita' barca: nx.approximation.maximum_independent_set(G1) Returns an approximate maximum independent set.
+        c=len(G1)-len(nx.approximation.maximum_independent_set(G1))-z
+        #scrivo valore su file
+        file=open(filename,"a")
+        file.write("\n!"+str(c))
+        file.close()
+        seed+=1
     z+=1
+    seed=0
