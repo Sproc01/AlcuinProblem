73fb69b63963e142551763bc7c98bbfba64026a1
Sproc01 <95143387+Sproc01@users.noreply.github.com>
2023-07-17 18:22:20 +0200

    Varying only the capacity from 0.9 random graph

--- Generatore grafi/RandomGeneratorDifficultGraph.py
+++ Generatore grafi/RandomGeneratorDifficultGraph.py
@@ -2,18 +2,18 @@ import networkx as nx
 
 z=0
 k=100
-p=[0.2,0.3,0.6,0.9]
-seed=[10,60,200]
-while z<3:
-    for i in p:
-        for j in seed:
-            G1=nx.gnp_random_graph(k,i,seed=j)
-            filename='GrafiCasuali/Graph'+str(int(i*100))+'_'+'_'+str(z)+'_'+str(j)+'.txt'
-            nx.write_adjlist(G1,filename)
-            #capacita' barca: nx.approximation.maximum_independent_set(G1) Returns an approximate maximum independent set.
-            c=len(G1)-len(nx.approximation.maximum_independent_set(G1))-z
-            #scrivo valore su file
-            file=open(filename,"a")
-            file.write("\n!"+str(c))
-            file.close()
+p=0.9
+seed=0
+while z<10:
+    #while seed<40:
+    G1=nx.gnp_random_graph(k,p,seed=seed)
+    filename='GrafiCasuali/Graph'+str(int(p*100))+'_'+str(z)+'_'+str(seed)+'.txt'
+    nx.write_adjlist(G1,filename)
+    #capacita' barca: nx.approximation.maximum_independent_set(G1) Returns an approximate maximum independent set.
+    c=len(G1)-len(nx.approximation.maximum_independent_set(G1))-z
+    #scrivo valore su file
+    file=open(filename,"a")
+    file.write("\n!"+str(c))
+    file.close()
+    #seed+=1
     z+=1
