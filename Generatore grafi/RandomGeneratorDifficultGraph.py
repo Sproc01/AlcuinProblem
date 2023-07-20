8efe45321770cb5cf7b252d8bf7558d43ff666e5
Sproc01 <95143387+Sproc01@users.noreply.github.com>
2023-07-20 16:29:38 +0200

    difficult graph

--- Generatore grafi/RandomGeneratorDifficultGraph.py
+++ Generatore grafi/RandomGeneratorDifficultGraph.py
@@ -1,13 +1,13 @@
 import networkx as nx
-
-z=0
+#Instanze difficili: (90,-1),(60,-2), (50,-2),(40,-2)
+z=1
 k=100
-p=0.1
+p=[0.9,0.6,0.5,0.4]
 seed=0
-while z<3:
-    while seed<20:
-        G1=nx.gnp_random_graph(k,p,seed=seed)
-        filename='GrafiCasuali/Graph'+str(int(p*100))+'_'+str(z)+'_'+str(seed)+'.txt'
+for i in p:
+    while seed<10:
+        G1=nx.gnp_random_graph(k,i,seed=seed)
+        filename='GrafiCasuali/Graph'+str(int(i*100))+'_'+str(z)+'_'+str(seed)+'.txt'
         nx.write_adjlist(G1,filename)
         #capacita' barca: nx.approximation.maximum_independent_set(G1) Returns an approximate maximum independent set.
         c=len(G1)-len(nx.approximation.maximum_independent_set(G1))-z
@@ -16,5 +16,5 @@ while z<3:
         file.write("\n!"+str(c))
         file.close()
         seed+=1
-    z+=1
+    z=2
     seed=0
