82032e9ecd37cd313c682f264842b68cee2d3c0f
Sproc01 <95143387+Sproc01@users.noreply.github.com>
2023-08-17 17:27:08 +0200

    Update elaboration.py

--- elaboration.py
+++ elaboration.py
@@ -15,16 +15,6 @@ plt.xlabel('Percentual edge')
 plt.ylabel('Time(s)')
 fig0_o.savefig('../Grafici/minCapacity=0 and optimal.png')
 
-fig0_i=plt.figure('minCapacity=0 and infeasible')
-probability=data[(data['MINCAPACITY']==0) & (data['TERMINATION CONDITION']=='infeasible')]['PROBABILITY']
-time=data[(data['MINCAPACITY']==0) & (data['TERMINATION CONDITION']=='infeasible')]['TIME(s)']
-plt.scatter(probability,time)
-sns.lineplot(x=probability,y=time)
-plt.title('minCapacity=0 and infeasible')
-plt.xlabel('Percentual edge')
-plt.ylabel('Time(s)')
-fig0_i.savefig('../Grafici/minCapacity=0 and infeasible')
-
 fig1_o=plt.figure('minCapacity=1 and optimal')
 probability=data[(data['TERMINATION CONDITION']=='optimal') & (data['MINCAPACITY']==1)]['PROBABILITY']
 time=data[(data['TERMINATION CONDITION']=='optimal') & (data['MINCAPACITY']==1)]['TIME(s)']
@@ -34,6 +24,26 @@ plt.title('minCapacity=1 and optimal')
 plt.xlabel('Percentual edge')
 plt.ylabel('Time(s)')
 fig1_o.savefig('../Grafici/minCapacity=1 and optimal.png')
+
+fig2_o=plt.figure('minCapacity=2 and optimal')
+probability=data[(data['MINCAPACITY']==2) & (data['TERMINATION CONDITION']=='optimal')]['PROBABILITY']
+time=data[(data['MINCAPACITY']==2) & (data['TERMINATION CONDITION']=='optimal')]['TIME(s)']
+plt.scatter(probability,time)
+sns.lineplot(x=probability,y=time)
+plt.title('minCapacity=2 and optimal')
+plt.xlabel('Percentual edge')
+plt.ylabel('Time(s)')
+fig2_o.savefig('../Grafici/minCapacity=2 and optimal.png')
+
+fig0_i=plt.figure('minCapacity=0 and infeasible')
+probability=data[(data['MINCAPACITY']==0) & (data['TERMINATION CONDITION']=='infeasible')]['PROBABILITY']
+time=data[(data['MINCAPACITY']==0) & (data['TERMINATION CONDITION']=='infeasible')]['TIME(s)']
+plt.scatter(probability,time)
+sns.lineplot(x=probability,y=time)
+plt.title('minCapacity=0 and infeasible')
+plt.xlabel('Percentual edge')
+plt.ylabel('Time(s)')
+fig0_i.savefig('../Grafici/minCapacity=0 and infeasible')
 
 fig1_i=plt.figure('minCapacity=1 and infeasible')
 probability=data[(data['MINCAPACITY']==1) & (data['TERMINATION CONDITION']=='infeasible')]['PROBABILITY']
@@ -55,20 +65,17 @@ plt.xlabel('Percentual edge')
 plt.ylabel('Time(s)')
 fig2_i.savefig('../Grafici/minCapacity=2 and infeasible.png')
 
-fig2_o=plt.figure('minCapacity=2 and optimal')
-probability=data[(data['MINCAPACITY']==2) & (data['TERMINATION CONDITION']=='optimal')]['PROBABILITY']
-time=data[(data['MINCAPACITY']==2) & (data['TERMINATION CONDITION']=='optimal')]['TIME(s)']
-plt.scatter(probability,time)
-sns.lineplot(x=probability,y=time)
-plt.title('minCapacity=2 and optimal')
-plt.xlabel('Percentual edge')
-plt.ylabel('Time(s)')
-fig2_o.savefig('../Grafici/minCapacity=2 and optimal.png')
-
 hist=plt.figure("Histogram optimal/infeasibile")
 terminationCondition=data['TERMINATION CONDITION']
 plt.hist(terminationCondition, bins=2, rwidth=0.9)
 hist.savefig('../Grafici/histogram.png')
+
+pie=plt.figure("Pie optimal/infeasibile")
+number=[]
+number.append(len(data[data['TERMINATION CONDITION']=='optimal']))
+number.append(len(data[data['TERMINATION CONDITION']=='infeasible']))
+plt.pie(number, labels=['optimal','infeasible'], shadow=True, startangle=90, explode=[0.1,0])
+pie.savefig('../Grafici/pie.png')
 
 minc_o=plt.figure("mincapacity=2 vs mincapacity=1 vs mincapacity=0 for optimal")
 minCapacityTime=data[data['TERMINATION CONDITION']=='optimal']['TIME(s)']
@@ -97,5 +104,3 @@ plt.title('mincapacity=2 vs mincapacity=1 vs mincapacity=0 for infeasible')
 plt.ylabel('Time(s)')
 plt.xlabel('Variation Capacity')
 fig7.savefig('../Grafici/mincapacity=2 vs mincapacity=1 vs mincapacity=0 for infeasible.png')
-
-#plt.show()
